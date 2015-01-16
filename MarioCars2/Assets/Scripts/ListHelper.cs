4423c03d164e017863a92eafad02fefa06c8bd1c
Alexander Biggs <alexanderkbiggs@gmail.com>
2015-01-16 18:28:17 -0500

    Level Manager

---
+++ MarioCars2/Assets/Scripts/ListHelper.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class ListHelper {
+	public static T Pop<T>(this IList<T> list, int index = 0) {
+		T value = list[index];
+		list.RemoveAt(index);
+		return value;
+	}
+}
