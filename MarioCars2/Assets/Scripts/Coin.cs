7d87c74ca6b4a73d6ae3f3938dfd513ec6fe8ae4
Daphne <daphneipp@gmail.com>
2015-01-16 18:29:54 -0500

    starting on a coin

---
+++ MarioCars2/Assets/Scripts/Coin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.rigidbody.MoveRotation(new Quaternion(0, 10, 0, 0));
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		/*if (collision == Player) {
+
+		}*/
+	}
+}
