9bb91d35cc9e8dc63da67b79d00ad1d952f5496f
Daphne <daphneipp@gmail.com>
2015-01-17 01:39:46 -0500

    Adding in sliding blocks.

--- MarioCars2/Assets/Scripts/Blocks/MovingBlock.cs
+++ MarioCars2/Assets/Scripts/Blocks/MovingBlock.cs
@@ -2,16 +2,49 @@
 using System.Collections;
 
 public class MovingBlock : MonoBehaviour {
+	//Note, you can only move in one direction at once (either horizontal or vertical)
 	public Vector3 moveDirection;
-	public float speed;
 
 	// Use this for initialization
 	void Start () {
-	
+		if ( this.isHorizontalMovement() ) {
+			this.rigidbody.constraints |= RigidbodyConstraints.FreezePositionY |
+										 RigidbodyConstraints.FreezeRotationX | 
+										 RigidbodyConstraints.FreezeRotationY |
+										 RigidbodyConstraints.FreezePositionZ;
+		}
+		else if ( this.isVerticalMovement() ) {
+			this.rigidbody.isKinematic = true;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (this.isHorizontalMovement()) {
+			this.rigidbody.velocity = moveDirection;
+		}
+	}
+
+	void FixedUpdate() {
+		if (this.isVerticalMovement()) {
+			Vector3 newPos = this.transform.position + (this.moveDirection * Time.deltaTime);
+			this.transform.position = newPos;
+		}
+	}
+
+	public void ChangeDirection() {
+		this.moveDirection = -this.moveDirection;
+
+		if (this.isHorizontalMovement()) {
+			this.rigidbody.velocity = this.moveDirection;
+		}
+	}
+
+	private bool isVerticalMovement() {
+		return this.moveDirection.y != 0;
+	}
+
+	private bool isHorizontalMovement() {
+		return this.moveDirection.x != 0 || this.moveDirection.z != 0;
 	}
 }
