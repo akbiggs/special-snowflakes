2059782c34724cdc77317c027de0fd56aee7e82d
Daphne <daphneipp@gmail.com>
2015-01-18 15:23:12 -0500

    rocket sound when hit

--- MarioCars2/Assets/Scripts/Objects/Goal.cs
+++ MarioCars2/Assets/Scripts/Objects/Goal.cs
@@ -5,15 +5,17 @@ public class Goal : MonoBehaviour {
 
 	public Animator animator;
 	bool activated = false;
-
+	
 	void OnCollisionEnter(Collision collision) {
 		Player player = collision.gameObject.GetComponent<Player>();
 		if (player != null && !this.activated) {
+			this.GetComponent<AudioSource>().Play();
+
 			this.activated = true;
 			this.animator.SetBool("On", true);
 
 			player.gameObject.SetActive(false);
-			Camera.main.gameObject.SetActive(false);			
+			Camera.main.gameObject.SetActive(false);	
 		}
 	}
 
