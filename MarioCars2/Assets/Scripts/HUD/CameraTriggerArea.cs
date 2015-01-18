a5ffc0fad5224c7ef2e8188f28ee4bd1ed4b5579
Adam <adamgryu@gmail.com>
2015-01-18 15:21:03 -0500

    worked on redux level

---
+++ MarioCars2/Assets/Scripts/HUD/CameraTriggerArea.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTriggerArea : MonoBehaviour {
+
+	private Vector3 newCameraOffset;
+	private Quaternion newCameraRotation;
+
+	void Start() {
+		Transform parameters = this.transform.FindChild("CameraParams");
+
+		this.newCameraOffset = parameters.position - this.transform.position;
+		this.newCameraRotation = parameters.rotation;
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Player player = collider.gameObject.GetComponent<Player>();
+		if (player != null) {
+			PlayerCamera pCamera = Camera.main.GetComponent<PlayerCamera>();
+			pCamera.difference = this.newCameraOffset;
+			pCamera.targetRotation = this.newCameraRotation;
+		}
+	}
+}
