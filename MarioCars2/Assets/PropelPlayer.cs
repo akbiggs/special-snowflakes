e4acc8c8df98a5f259a865f3c3a1efd0c91f9746
Alexander Biggs <alexanderkbiggs@gmail.com>
2015-01-17 21:03:42 -0500

    Work on bouncy level with slide

---
+++ MarioCars2/Assets/PropelPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropelPlayer : MonoBehaviour {
+
+	public Vector3 amount;
+
+	public void OnTriggerEnter(Collider other) {
+		Player player = other.gameObject.GetComponent<Player>();
+		if (player != null) {
+			player.gameObject.rigidbody.velocity = amount;
+		}
+	}
+}
