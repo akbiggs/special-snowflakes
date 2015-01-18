38ef5eddc5bd37da1cecaded11553ea5ac5e4561
Alexander Biggs <alexanderkbiggs@gmail.com>
2015-01-18 15:20:07 -0500

    Start work on hub level

---
+++ MarioCars2/Assets/InputText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputText : MonoBehaviour {
+	public string JoypadText;
+	public string KeyboardText;
+
+	void Start() {
+		string[] names = Input.GetJoystickNames();
+		if (names.Length == 0) {
+			this.GetComponent<TextMesh>().text = this.KeyboardText;
+		} else {
+			this.GetComponent<TextMesh>().text = this.JoypadText;
+		}
+	}
+}
---
+++ MarioCars2/Assets/ShowWhenNear.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowWhenNear : MonoBehaviour {
+	public float distance;
+	public GameObject checkObject;
+	public float showSpeed = 0.5f;
+
+	private Vector3 showPosition;
+	private Vector3 hidePosition;
+	private bool showing = false;
+
+	void Start() {
+		this.showPosition = this.transform.position;
+		this.transform.position = this.transform.FindChild("HideAnchor").position;
+		this.hidePosition = this.transform.position;
+	}
+
+	void Update() {
+		if (this.checkObject == null) {
+			return;
+		}
+
+		if (!showing && Vector3.Distance(this.showPosition, checkObject.transform.position) <= this.distance) {
+			this.showing = true;
+		} else if (showing) {
+			this.transform.position = Vector3.MoveTowards(this.transform.position, this.showPosition,
+			                                              Vector3.Distance(this.transform.position, this.showPosition) / Vector3.Distance(this.hidePosition, this.showPosition));
+		}
+	}
+}
